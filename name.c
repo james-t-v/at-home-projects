84f03be9e7b336b3b450be569b787c313fb6ba95
james-t-v <116467658+james-t-v@users.noreply.github.com>
2022-10-29 17:27:02 -0700

    Now, the code gives an actual array.

--- name.c
+++ name.c
@@ -6,9 +6,9 @@ int main()
 	printf("Cuantos numeros usas?");
 	scanf("%i", &x);
 	
-	for(int i=0; x>i; i++)
+	for(int i=0; i<=x; i++)
 	{
-		for(int k=i; k>i; k--)
+		for(int k=i; k>0; k--)
 		{
 			printf(" %i", k);
 		}
